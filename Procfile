74c9b093d077a48a628180ebe5d8909ee795ef9c
atinachang <atinachang@gmail.com>
2020-12-13 21:00:02 -0500

    updated code for deployment

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 web: node server.js
-=======
-web: node './api/server.js'
->>>>>>> e9829252932a361cbb469be98ea0709ff8000342
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
-// const cookieParser = require('cookie-parser');
-
-//mongodb+srv://admin:53RCx09szrKdZ0OK@shows.c8z3g.mongodb.net/ISO?retryWrites=true&w=majority
-
+const cookieParser = require('cookie-parser')
 // bring in mongoose
 const mongoose = require('mongoose');
 
@@ -11,12 +8,15 @@ const mongoose = require('mongoose');
 const path = require("path");
 
 // connection string to LOCAL DB
-const DB_URI = 'mongodb://localhost:27017/radio-archive';
+// const DB_URI = 'mongodb+srv://admin:HSoeJSaYnVTktjwd@cluster0.njomo.mongodb.net/radio-archive?retryWrites=true&w=majority';
+const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/radio-archive';
+// const DB_URI = 'mongodb://localhost:27017/radio-archive'
 // where are SERVER will run, not DB
 const PORT = process.env.PORT || '5000';
+// const PORT = '5000'
 
-const showRouter = require('./routes/archives');
-const userRouter = require('./routes/users')
+const showRouter = require('./api/routes/archives');
+const userRouter = require('./api/routes/users')
 
 // Connect to database
 mongoose
@@ -35,7 +35,7 @@ mongoose
     console.log(err.message);
   })
 
-// app.use(cookieParser())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
 // Express body parser middleware
@@ -50,14 +50,14 @@ app.use('/api/users', userRouter)
 
 // server.js at the very end of the file.
 // console.log(process.env.NODE_ENV)
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('./build'));
-//     // only add this part if you are using React Router
-//     app.get('*', (req,res) =>{
-//         console.log(path.join(__dirname+'/build/index.html'));
-//         res.sendFile(path.join(__dirname+'/build/index.html'));
-//     });
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./build'));
+    // only add this part if you are using React Router
+    app.get('*', (req,res) =>{
+        console.log(path.join(__dirname+'/build/index.html'));
+        res.sendFile(path.join(__dirname+'/build/index.html'));
+    });
+}
 
 // Start server
 app.listen(PORT, () => {
