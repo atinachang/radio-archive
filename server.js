bc58ffc501eee159b8d169dda40c343bde10bf18
atinachang <atinachang@gmail.com>
2020-12-13 21:11:09 -0500

    updates before deployment

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const app = express();
 const cookieParser = require('cookie-parser')
 // bring in mongoose
 const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // bring in path?
 const path = require("path");
 
 // connection string to LOCAL DB
 // const DB_URI = 'mongodb+srv://admin:HSoeJSaYnVTktjwd@cluster0.njomo.mongodb.net/radio-archive?retryWrites=true&w=majority';
-const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/radio-archive';
+const DB_URI = process.env.DB_URI;
+// || 'mongodb://localhost:27017/radio-archive';
 // const DB_URI = 'mongodb://localhost:27017/radio-archive'
 // where are SERVER will run, not DB
 const PORT = process.env.PORT || '5000';
@@ -18,22 +22,7 @@ const PORT = process.env.PORT || '5000';
 const showRouter = require('./api/routes/archives');
 const userRouter = require('./api/routes/users')
 
-// Connect to database
-mongoose
-  .connect(DB_URI,     
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    },
 
-    )
-  .then(() => {
-    console.log(`Successfully connected to: ${DB_URI} `);
-  })
-  .catch( err => {
-    console.log(err.message);
-  })
 
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
