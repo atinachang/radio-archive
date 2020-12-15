88b459d8cc863267756d10f40e7d96c24fddb21a
atinachang <atinachang@gmail.com>
2020-12-14 22:43:33 -0500

    pls work

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ const path = require("path");
 const showRouter = require('./api/routes/archives');
 const userRouter = require('./api/routes/users')
 
+
 // dotenv.config();
 // console.log(dotenv.config())
 // connection string to LOCAL DB
-// const DB_URI = 'mongodb+srv://admin:HSoeJSaYnVTktjwd@cluster0.njomo.mongodb.net/radio-archive?retryWrites=true&w=majority';
-// const DB_URI = process.env.DB_URI;
-// || 'mongodb://localhost:27017/radio-archive';
-const DB_URI = 'mongodb://localhost:27017/radio-archive';
+// const DB_URI = 'mongodb+srv://admin:3P1W5Pazgq1f6SZy@shows.c8z3g.mongodb.net/ISOretryWrites=true&w=majority';
+const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/radio-archive';
+// const DB_URI = 'mongodb://localhost:27017/radio-archive';
 // where are SERVER will run, not DB
-// const PORT = process.env.PORT || '5000';
-const PORT = 5000
+const PORT = process.env.PORT || '5000';
+// const PORT = 5000
 
 
 app.use(cookieParser())
